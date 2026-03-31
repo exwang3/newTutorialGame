9b815667a41f65fc8ce95447d36001204d9d6cc3
Eric Wang <exwang3@gmail.com>
2026-03-31 23:18:06 +1300

    Updated script - size

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,7 +5,8 @@ public class Obstacle : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        float randomSize = Random.Range(0.5f, 2.0f);
+        transform.localScale = new Vector3(randomSize, randomSize, 1);
     }
 
     // Update is called once per frame
