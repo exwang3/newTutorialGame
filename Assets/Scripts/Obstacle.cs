52336a8703163af33564a3c3bf5ee4975bbe52db
Eric Wang <exwang3@gmail.com>
2026-04-01 23:11:56 +1300

    Updated script-randomness

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,16 +2,25 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    public float minSize = 0.5f;
+    public float maxSize = 2.0f;
+    public float minSpeed = 50f;
+    public float maxSpeed = 150f;
+    Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        float randomSize = Random.Range(0.5f, 2.0f);
+        float randomSize = Random.Range(minSize, maxSize);
         transform.localScale = new Vector3(randomSize, randomSize, 1);
+        rb = GetComponent<Rigidbody2D>();
+        float randomSpeed = Random.Range(minSpeed, maxSpeed);
+        Vector2 randomDirection = Random.insideUnitCircle;
+        rb.AddForce(randomDirection * randomSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
