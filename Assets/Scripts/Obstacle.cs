fc97b96fb1f2b684ecf124966d79dd0bc119c777
Eric Wang <exwang3@gmail.com>
2026-04-02 23:33:19 +1300

    added player object

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    public float maxSpinSpeed = 10f;
     public float minSize = 0.5f;
     public float maxSize = 2.0f;
     public float minSpeed = 50f;
@@ -12,10 +13,15 @@ public class Obstacle : MonoBehaviour
     {
         float randomSize = Random.Range(minSize, maxSize);
         transform.localScale = new Vector3(randomSize, randomSize, 1);
+
         rb = GetComponent<Rigidbody2D>();
-        float randomSpeed = Random.Range(minSpeed, maxSpeed);
+
+        float randomSpeed = Random.Range(minSpeed, maxSpeed) / randomSize;
         Vector2 randomDirection = Random.insideUnitCircle;
         rb.AddForce(randomDirection * randomSpeed);
+
+        float randomTorque = Random.Range(-maxSpinSpeed, maxSpinSpeed);
+        rb.AddTorque(randomTorque);
     }
 
     // Update is called once per frame
